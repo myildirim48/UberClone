8a1616f5e0ca5a5a564f91d094c1b712e3b2cbcf
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-04-21 22:26:18 +0300

    Initial Commit

---
+++ UberClone-SwiftUI/UberClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberClone_SwiftUIApp.swift
+//  UberClone-SwiftUI
+//
+//  Created by YILDIRIM on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
